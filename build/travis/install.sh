4f8049c0321d639673a5f7c877d8a355f57e5f65
heddn <lucashedding@1463982.no-reply.drupal.org>
2016-03-29 21:45:48 -0600

    update phantomjs travis config

--- build/travis/install.sh
+++ build/travis/install.sh
@@ -5,12 +5,3 @@ composer --verbose --prefer-dist install
 phpenv rehash
 cd ${TRAVIS_BUILD_DIR}/web
 ${TRAVIS_BUILD_DIR}/vendor/bin/drush site-install config_installer -y --verbose --db-url=mysql://root:@127.0.0.1/travis --db-su='root' --db-su-pw=''
-
-# Install phantomjs
-sudo apt-get update; sudo apt-get install libicu52
-cd ${TRAVIS_BUILD_DIR}
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar jxf phantomjs-2.1.1-linux-x86_64.tar.bz2
-chmod 755 ${TRAVIS_BUILD_DIR}/phantomjs-2.1.1-linux-x86_64/bin/phantomjs
-sudo ln -s --force ~/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
-mkdir /tmp/pjsdrivercache/phantomjs
