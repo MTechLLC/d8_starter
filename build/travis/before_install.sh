f13a426b7dcbd6ed25da7f297c5986a72413081b
heddn <lucashedding@1463982.no-reply.drupal.org>
2016-03-30 09:31:34 -0600

    improve on vendor caching

--- build/travis/before_install.sh
+++ build/travis/before_install.sh
@@ -8,6 +8,7 @@ echo sendmail_path=`which true` >> ~/.phpenv/versions/$(phpenv version-name)/etc
 if [[ ! -d ${HOME}/vendor-cache ]]; then mkdir ${HOME}/vendor-cache; fi
 echo "Vendor cache content:"
 ls -lh ~/vendor-cache/
+cd ${TRAVIS_BUILD_DIR}
 cachefile="`echo -n ${TRAVIS_PHP_VERSION} | sha1sum | cut -d " " -f 1`.tar"
 if [[ -f ~/vendor-cache/${cachefile} ]]; then tar -xf ~/vendor-cache/${cachefile}; fi
 if [[ -d ~/vendor/ ]]; then echo "Size of vendor directory extracted from cache:"; du -hs ~/vendor/; else echo "Vendor directory does not exist"; fi
