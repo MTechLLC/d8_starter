dbc5d9ea7252a71bc8851efdaa6d0bc23b214a55
Edys Meza <edysmeza@gmail.com>
2017-11-10 16:07:36 -0600

    fixing composer requirements

--- build/travis/install.sh
+++ build/travis/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ev
-composer --verbose --prefer-dist install
+composer --verbose --prefer-dist --ignore-platform-reqs install
 cd ${TRAVIS_BUILD_DIR}/web
 printf "<?php\n\$databases['default']['default'] = ['database' => 'tmp/site.sqlite', 'prefix' => '', 'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite', 'driver' => 'sqlite'];" > ${TRAVIS_BUILD_DIR}/web/sites/default/settings.local.php
 ${TRAVIS_BUILD_DIR}/vendor/bin/drush site-install --verbose --yes config_installer --db-url=sqlite://tmp/site.sqlite
